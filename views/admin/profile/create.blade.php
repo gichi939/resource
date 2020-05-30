7a4735654f8e5e3d07ca0072b958e87febc42b90
”名前” <>
2020-05-30 00:03:27 +0000

    2020 5 30

---
+++ views/admin/profile/create.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.profile')
+
+
+@section('title', 'my https://github.com/gichi939/mynews')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>my プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
